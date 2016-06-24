a48cfb1451234a9abeb8df89c79b9a700b67a2c6
forsrc <forsrc@163.com>
2016-06-25 00:22:42 +0900

    config

--- src/main/java/com/forsrc/springmvc/restful/user/controller/UserRestfulController.java
+++ src/main/java/com/forsrc/springmvc/restful/user/controller/UserRestfulController.java
@@ -4,10 +4,7 @@ import com.forsrc.pojo.User;
 import com.forsrc.springmvc.restful.user.service.UserRestfulService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
@@ -18,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+@RestController
 @Controller
 @RequestMapping(value = "/v1.0")
 public class UserRestfulController {
