c31dd0fb0bbc6e644e8dc8a98cb839d07b89b036
forsrc <forsrc@163.com>
2016-06-29 20:40:29 +0900

    cordova platform add

---
+++ src/main/webapp/MyStudy/platforms/android/assets/www/js/web/logout.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function () {
+
+    app.initialize();
+    $.material.init();
+    $.material.ripples();
+    sessionStorage.removeItem('activeTab');
+    sessionStorage.removeItem('leadId');
+
+    sessionStorage.clear();
+    window.location.href = MY_WEB_URL.login;
+});
