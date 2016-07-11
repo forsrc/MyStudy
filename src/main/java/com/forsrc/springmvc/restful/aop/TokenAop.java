a14bc2d2815ad74c5edb082a8955386ea6bbef62
forsrc <forsrc@163.com>
2016-07-11 12:02:59 +0900

    AOP

--- src/main/java/com/forsrc/springmvc/restful/aop/TokenAop.java
+++ src/main/java/com/forsrc/springmvc/restful/aop/TokenAop.java
@@ -2,15 +2,19 @@ package com.forsrc.springmvc.restful.aop;
 
 
 import com.forsrc.aop.LogTracing;
+import com.forsrc.constant.KeyConstants;
+import com.forsrc.constant.MyToken;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TokenAop {
 
@@ -19,25 +23,43 @@ public class TokenAop {
     public void doAfter(JoinPoint jp) {
     }
 
-    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 
         if (RequestContextHolder.getRequestAttributes() == null) {
-            return pjp.proceed();
+            return proceedingJoinPoint.proceed();
         }
-        HttpServletRequest request =
-                ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpServletRequest request = null;
+        Object[] args = proceedingJoinPoint.getArgs();
+        for (Object object : args) {
+            if (object instanceof HttpServletRequest) {
+                request = (HttpServletRequest) object;
+            }
+        }
+        //request =  ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         if (request == null) {
-            return pjp.proceed();
+            return proceedingJoinPoint.proceed();
         }
         HttpSession session = request.getSession();
         if (session == null) {
-            return pjp.proceed();
+            return proceedingJoinPoint.proceed();
         }
+        ModelAndView modelAndView = new ModelAndView();
+        Map<String, String> message = new HashMap<String, String>();
+        modelAndView.addObject("return", message);
+        modelAndView.addObject("status", 401);
         String token = request.getParameter("token");
+
         if(StringUtils.isBlank(token)){
-            return null;
+            message.put("message", "Missing token");
+            return modelAndView;
         }
-        return pjp.proceed();
+        MyToken myToken = (MyToken) session.getAttribute(KeyConstants.TOKEN.getKey());
+        if (token.equals(myToken.getToken())) {
+            message.put("message", "Invalid token");
+            return modelAndView;
+        }
+
+        return proceedingJoinPoint.proceed();
     }
 
     public void doBefore(JoinPoint jp) {
